6aef187413f5c97d8ee845867b6348089214b3b3
cathciyal <aruna.cathciyal24@gmail.com>
2023-07-25 09:07:16 +0000

    some changes

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import Login from "./Components/Login" */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Service from "./pages/Service";
+import Navbar1 from "./NavComponents/Navbar1"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
  <App/>
 );
-
 
 
 
