0a8abba904235e360376bd63853fa2818eeac8a1
Bibek Chakraborty <bibekbibek0612@gmail.com>
2023-07-30 21:22:12 +0530

    modified spring application

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,13 +1,43 @@
-package com.examly.springapp;
+// package com.examly.springapp;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class SpringappApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(SpringappApplication.class, args);
+// 	}
+
+// }
+
+package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("*");
+			}
+		};
 	}
 
 }
