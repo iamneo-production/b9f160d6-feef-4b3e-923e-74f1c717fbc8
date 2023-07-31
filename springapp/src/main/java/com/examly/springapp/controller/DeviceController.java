d33178b58e5af09b9bba8fad2b1224c219941744
cathciyal <aruna.cathciyal24@gmail.com>
2023-07-31 06:38:52 +0000

    Added personal port

--- springapp/src/main/java/com/examly/springapp/controller/DeviceController.java
+++ springapp/src/main/java/com/examly/springapp/controller/DeviceController.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/devices")
-@CrossOrigin
+@CrossOrigin("https://8081-dccdbaeddaaddcfecfeacffbbbdffeaeaadbdbabf.project.examly.io")
 public class DeviceController {
 
     @Autowired
